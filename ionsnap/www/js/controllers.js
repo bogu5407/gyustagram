a90b0314c759de636a44df659b4c493fadcb22cf
BG <bogu5407@nate.com>
2017-02-09 02:52:40 +0200

    insta

---
+++ ionsnap/www/js/controllers.js
@@ -0,0 +1,29 @@
+angular.module('starter.controllers', [])
+
+
+
+.controller('DashCtrl', function ($scope) { })
+
+
+
+.controller('ChatsCtrl', function ($scope, Chats) {
+
+
+
+})
+
+
+
+.controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+
+
+
+})
+
+
+
+.controller('AccountCtrl', function ($scope) {
+
+
+
+});
